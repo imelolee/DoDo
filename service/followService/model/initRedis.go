f9b66ca55ab3d56a6dd5fae0c47261d42da5fd60
imelolee <wataruri1129@gmail.com>
2022-08-22 21:34:20 +0800

    likeService

---
+++ service/followService/model/initRedis.go
@@ -0,0 +1,12 @@
+package model
+
+import (
+	"context"
+)
+
+var Ctx = context.Background()
+
+// InitRedis 初始化Redis连接。
+func InitRedis() {
+
+}
--- service/likeService/main.go
+++ service/likeService/main.go
@@ -1,30 +1,41 @@
 package main
 
 import (
-
+	"fmt"
+	"github.com/go-micro/plugins/v4/registry/consul"
 	"likeService/handler"
+	"likeService/model"
 	pb "likeService/proto"
 
 	"go-micro.dev/v4"
 	log "go-micro.dev/v4/logger"
-
 )
 
 var (
-	service = "likeservice"
+	service = "likeService"
 	version = "latest"
 )
 
 func main() {
+	model.InitRedis()
+	model.InitDb()
+	model.InitRabbitMQ()
+
+	consulReg := consul.NewRegistry()
+
 	// Create service
 	srv := micro.NewService(
 		micro.Name(service),
 		micro.Version(version),
+		micro.Registry(consulReg),
 	)
-	srv.Init()
 
 	// Register handler
-	pb.RegisterLikeServiceHandler(srv.Server(), new(handler.LikeService))
+	err := pb.RegisterLikeServiceHandler(srv.Server(), new(handler.LikeService))
+	if err != nil {
+		fmt.Println("RegisterHandler err: ", err)
+		return
+	}
 	// Run service
 	if err := srv.Run(); err != nil {
 		log.Fatal(err)
