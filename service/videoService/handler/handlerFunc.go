2360a7a3b88f0f79807d3530ee0da8bf98e241b3
genleel <imelolee@gmail.com>
2022-08-31 15:33:26 +0800

    basic api

--- service/videoService/handler/handlerFunc.go
+++ service/videoService/handler/handlerFunc.go
@@ -9,6 +9,7 @@ import (
 	"github.com/qiniu/go-sdk/v7/sms/bytes"
 	"github.com/qiniu/go-sdk/v7/storage"
 	"go-micro.dev/v4"
+	log "go-micro.dev/v4/logger"
 	likeService "likeService/proto"
 	"sync"
 	userModel "userService/model"
@@ -43,48 +44,59 @@ func creatVideo(video *model.FeedVideo, data *model.Video, userId int64) {
 	//插入Author，这里需要将视频的发布者和当前登录的用户传入，才能正确获得isFollow，
 	//如果出现错误，不能直接返回失败，将默认值返回，保证稳定
 
-	micro := InitMicro()
-	userClient := userService.NewUserService("userService", micro.Client())
-	likeClient := likeService.NewLikeService("likeService", micro.Client())
-	commentClient := commentService.NewCommentService("commentService", micro.Client())
-
 	go func() {
-		userRsp, _ := userClient.GetFeedUserByIdWithCurId(context.TODO(), &userService.CurIdReq{
+		userMicro := InitMicro()
+		userClient := userService.NewUserService("userService", userMicro.Client())
+		userRsp, err := userClient.GetFeedUserByIdWithCurId(context.TODO(), &userService.CurIdReq{
 			Id:    data.AuthorId,
 			CurId: userId,
-		})
+		}, config.Opts)
 
-		var tmpUser userModel.FeedUser
+		if err != nil {
+			log.Infof("userClient.GetFeedUserByIdWithCurId err:", err)
+		}
+
+		var tmpUser *userModel.FeedUser
 		gconv.Struct(userRsp.User, &tmpUser)
 
-		video.Author = tmpUser
+		video.Author = *tmpUser
 		wg.Done()
 	}()
 
 	//插入点赞数量，同上所示，不将nil直接向上返回，数据没有就算了，给一个默认就行了
 	go func() {
+		likeMicro := InitMicro()
+		likeClient := likeService.NewLikeService("likeService", likeMicro.Client())
 		likeRsp, _ := likeClient.FavouriteCount(context.TODO(), &likeService.IdReq{
 			Id: data.Id,
-		})
+		}, config.Opts)
 		video.FavoriteCount = likeRsp.Count
 		wg.Done()
 	}()
 
 	//获取该视屏的评论数字
 	go func() {
+		commentMicro := InitMicro()
+		commentClient := commentService.NewCommentService("commentService", commentMicro.Client())
 		commentRsp, _ := commentClient.CountFromVideoId(context.TODO(), &commentService.IdReq{
 			Id: data.Id,
-		})
+		}, config.Opts)
 		video.CommentCount = commentRsp.Count
 		wg.Done()
 	}()
 
 	//获取当前用户是否点赞了该视频
 	go func() {
-		likeRsp, _ := likeClient.IsFavorite(context.TODO(), &likeService.VideoUserReq{
+		likeMicro := InitMicro()
+		likeClient := likeService.NewLikeService("likeService", likeMicro.Client())
+		likeRsp, err := likeClient.IsFavorite(context.TODO(), &likeService.VideoUserReq{
 			VideoId: video.Id,
 			UserId:  userId,
-		})
+		}, config.Opts)
+		if err != nil {
+			log.Infof("likeClient.IsFavorite err:", err)
+		}
+
 		video.IsFavorite = likeRsp.Flag
 		wg.Done()
 	}()
