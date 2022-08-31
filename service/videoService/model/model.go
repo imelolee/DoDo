1cafc8f4c55faede4c7818a0945eaf67452739e6
imelolee <wataruri1129@gmail.com>
2022-08-31 15:33:26 +0800

    basic api

--- service/videoService/model/model.go
+++ service/videoService/model/model.go
@@ -14,6 +14,17 @@ type Video struct {
 	Title       string `json:"title"` //视频名，5.23添加
 }
 
+type PublishVideo struct {
+	Id            int64                  `json:"id"`
+	Author        *userModel.PublishUser `protobuf:"bytes,2,opt,name=author,proto3" json:"author,omitempty"`
+	PlayUrl       string                 `json:"play_url"`
+	CoverUrl      string                 `json:"cover_url"`
+	FavoriteCount int64                  `json:"favorite_count"`
+	CommentCount  int64                  `json:"comment_count"`
+	IsFavorite    bool                   `json:"is_favorite"`
+	Title         string                 `json:"title"`
+}
+
 type FeedVideo struct {
 	Video
 	Author        userModel.FeedUser `json:"author"`
